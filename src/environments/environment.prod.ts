f98b6c4fab967aecb4c77a5a6aa2afc2936541be
Michael Ocampo <imboocampo19@gmail.com>
2020-08-08 21:14:07 +0800

    Environt Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDnWLubDs9BRuL40Q0OkIv1u6HNHyjOw48",
+    authDomain: "makasora-template.firebaseapp.com",
+    databaseURL: "https://makasora-template.firebaseio.com",
+    projectId: "makasora-template",
+    storageBucket: "makasora-template.appspot.com",
+    messagingSenderId: "606200529542",
+    appId: "1:606200529542:web:e324a449f97c401d5b0707",
+    measurementId: "G-VLE55HCP3L"
+  },
 };
