9992d236326d3ffa19e760e1ede309ea48129528
Michael Ocampo <johnmichaelocampo1630@gmail.com>
2020-08-10 22:38:01 +0800

    Auth Guard

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -11,47 +11,49 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class AuthService {
-  user$: Observable<User>;
+  user$: Observable<any>;
+  private postBasePath: string = '/users';
+  Users: AngularFireList<User> = null; //  list of objects
   constructor(
     public angularFireAuth: AngularFireAuth,
     private angularFireDatabase: AngularFireDatabase,
     public router: Router,
   ) {
+    this.Users = this.angularFireDatabase.list(this.postBasePath);
     this.user$ = this.angularFireAuth.authState.pipe(
       switchMap(user => {
-          // Logged in
         if (user) {
           return this.angularFireDatabase.object(`users/${user.uid}`).valueChanges();
         } else {
-          // Logged out
           return of(null);
         }
       })
     );
-    this.angularFireAuth.authState.subscribe(res =>  console.log(res) );
    }
 
-  login() {
-    const credential = this.angularFireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    credential.then(res => this.updateUserData(res.user));
+  async login() {
+    const credential = await this.angularFireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.updateUserData(credential.user)
   }
   logout() {
     this.angularFireAuth.auth.signOut();
   }
   
   private updateUserData(user) {
-    // Sets user data to firestore on login
-    const itemPath =  `$user/${user.uid}`;
-    const userRef: AngularFireObject<User>  = this.angularFireDatabase.object(itemPath)
-    const data: User = { 
-      uid: user.uid, 
-      email: user.email, 
-      displayName: user.displayName, 
-      photoURL: user.photoURL
-    } 
-
-    return userRef.set(data)
-
+    this.user$.subscribe(res =>  {
+      if (res) {
+        this.Users.update(res.key, res.payload.val())
+      .catch(error => console.log(error));
+      }else {
+        const data: User = { 
+          uid: user.uid, 
+          email: user.email, 
+          displayName: user.displayName, 
+          photoURL: user.photoURL
+        } 
+        this.Users.set(user.uid, data);
+      }
+    });
   }
   
 }
